9d8cc4b230fed51922a15365b74509368f1ce4d7
Grigory Petrov <grigory.v.p@gmail.com>
2012-08-22 00:01:26 +0400

    Standard install facility.

---
+++ pd/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Python DSL
+# Copyright 2012 Grigory Petrov
+# See LICENSE for details.
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding:utf-8 vi:et:ts=2
+
+from distutils.core import setup
+
+setup(
+  name='pd',
+  version='0.1.0',
+  description='Simple Python DSL framework.',
+  author='Grigory Petrov',
+  author_email='grigory.v.p@gmail.com',
+  url='http://bitbucket.org/eyeofhell/pd/',
+  packages=[ 'pd' ],
+  classifiers=[
+    'Development Status :: 1 - Prototype',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: GPLv3',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Framework' ])
+
